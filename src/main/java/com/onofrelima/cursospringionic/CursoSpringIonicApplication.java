69ba11754ed128ec88693556392c3c7f1d1e19ec
onofresergio <onofresergio@yahoo.com.br>
2021-04-18 18:55:28 -0300

    commit inicial

---
+++ src/main/java/com/onofrelima/cursospringionic/CursoSpringIonicApplication.java
@@ -0,0 +1,13 @@
+package com.onofrelima.cursospringionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringIonicApplication.class, args);
+	}
+
+}
