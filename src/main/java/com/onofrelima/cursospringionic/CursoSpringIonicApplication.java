a1e1cf0f244c5777512ca9f9abc2667d9cba8bc7
onofresergio <onofresergio@yahoo.com.br>
2021-04-18 21:09:49 -0300

    criando operacao de instanciacao inicial de dados na base h2

--- src/main/java/com/onofrelima/cursospringionic/CursoSpringIonicApplication.java
+++ src/main/java/com/onofrelima/cursospringionic/CursoSpringIonicApplication.java
@@ -1,13 +1,32 @@
 package com.onofrelima.cursospringionic;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.onofrelima.cursospringionic.domain.Categoria;
+import com.onofrelima.cursospringionic.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursoSpringIonicApplication {
+public class CursoSpringIonicApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringIonicApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
