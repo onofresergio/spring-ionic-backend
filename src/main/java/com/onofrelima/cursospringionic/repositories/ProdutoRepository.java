411e22011c3b36185c046d7c5993744f9508d0d6
onofresergio <onofresergio@yahoo.com.br>
2021-04-19 21:12:58 -0300

    produto e associacao muitos para muitos

---
+++ src/main/java/com/onofrelima/cursospringionic/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.onofrelima.cursospringionic.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.onofrelima.cursospringionic.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
