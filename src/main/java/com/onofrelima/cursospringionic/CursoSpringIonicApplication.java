1fa9cbd67630dfeb6912334d01b08cfa7ea5ea5e
onofresergio <onofresergio@yahoo.com.br>
2021-04-21 18:57:57 -0300

    criacao de entidades Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/onofrelima/cursospringionic/CursoSpringIonicApplication.java
+++ src/main/java/com/onofrelima/cursospringionic/CursoSpringIonicApplication.java
@@ -1,5 +1,6 @@
 package com.onofrelima.cursospringionic;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.onofrelima.cursospringionic.domain.Cidade;
 import com.onofrelima.cursospringionic.domain.Cliente;
 import com.onofrelima.cursospringionic.domain.Endereco;
 import com.onofrelima.cursospringionic.domain.Estado;
+import com.onofrelima.cursospringionic.domain.Pagamento;
+import com.onofrelima.cursospringionic.domain.PagamentoComBoleto;
+import com.onofrelima.cursospringionic.domain.PagamentoComCartao;
+import com.onofrelima.cursospringionic.domain.Pedido;
 import com.onofrelima.cursospringionic.domain.Produto;
+import com.onofrelima.cursospringionic.domain.enums.EstadoPagamento;
 import com.onofrelima.cursospringionic.domain.enums.TipoCliente;
 import com.onofrelima.cursospringionic.repositories.CategoriaRepository;
 import com.onofrelima.cursospringionic.repositories.CidadeRepository;
 import com.onofrelima.cursospringionic.repositories.ClienteRepository;
 import com.onofrelima.cursospringionic.repositories.EnderecoRepository;
 import com.onofrelima.cursospringionic.repositories.EstadoRepository;
+import com.onofrelima.cursospringionic.repositories.PagamentoRepository;
+import com.onofrelima.cursospringionic.repositories.PedidoRepository;
 import com.onofrelima.cursospringionic.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursoSpringIonicApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringIonicApplication.class, args);
@@ -85,6 +97,22 @@ public class CursoSpringIonicApplication implements CommandLineRunner {
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+				ped2.setPagamento(pagto2);
+				
+				pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+				pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 	}
 
 }
