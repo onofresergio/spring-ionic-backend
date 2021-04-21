7fde1dbe429080426cd934e5682451713e0a3a62
onofresergio <onofresergio@yahoo.com.br>
2021-04-21 17:11:15 -0300

    Adicionado entidades Cliente, Endereco, Telefone

--- src/main/java/com/onofrelima/cursospringionic/CursoSpringIonicApplication.java
+++ src/main/java/com/onofrelima/cursospringionic/CursoSpringIonicApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.onofrelima.cursospringionic.domain.Categoria;
 import com.onofrelima.cursospringionic.domain.Cidade;
+import com.onofrelima.cursospringionic.domain.Cliente;
+import com.onofrelima.cursospringionic.domain.Endereco;
 import com.onofrelima.cursospringionic.domain.Estado;
 import com.onofrelima.cursospringionic.domain.Produto;
+import com.onofrelima.cursospringionic.domain.enums.TipoCliente;
 import com.onofrelima.cursospringionic.repositories.CategoriaRepository;
 import com.onofrelima.cursospringionic.repositories.CidadeRepository;
+import com.onofrelima.cursospringionic.repositories.ClienteRepository;
+import com.onofrelima.cursospringionic.repositories.EnderecoRepository;
 import com.onofrelima.cursospringionic.repositories.EstadoRepository;
 import com.onofrelima.cursospringionic.repositories.ProdutoRepository;
 
@@ -27,6 +32,10 @@ public class CursoSpringIonicApplication implements CommandLineRunner {
 	private EstadoRepository estadoRepository;
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringIonicApplication.class, args);
@@ -64,6 +73,18 @@ public class CursoSpringIonicApplication implements CommandLineRunner {
 		
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "123456789000", TipoCliente.PESSOAFISICA);
+		
+		cli1.getTelefones().addAll(Arrays.asList("222233333", "999988876"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "sala 800", "Centro", "55220834", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 	}
 
 }
---
+++ src/main/java/com/onofrelima/cursospringionic/domain/enums/TipoCliente.java
@@ -0,0 +1,45 @@
+package com.onofrelima.cursospringionic.domain.enums;
+
+public enum TipoCliente {
+	
+	PESSOAFISICA(1, "Pessoa Física"),
+	PESSOAJURIDICA(2, "PessoanJurídica");
+	
+	private Integer cod;
+	private String descricao;
+	
+	private TipoCliente(Integer cod, String descricao) {
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+
+	public Integer getCod() {
+		return cod;
+	}
+
+	public void setCod(Integer cod) {
+		this.cod = cod;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public static TipoCliente toEnum(Integer cod) {
+		if(cod == null) {
+			return null;
+		}
+		
+		for(TipoCliente x : TipoCliente.values()) {
+			if (cod.equals(x.getCod())) {
+				return x;
+			}
+		}
+		
+		throw new IllegalArgumentException("Id inválido " + cod);
+	}
+}
